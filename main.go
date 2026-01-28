d33f8d0895fc0730b7dc9099dc533ba2a7017041
Azissa <azisagni28@gmail.com>
2026-01-28 12:54:14 +0800

    adding input username to cli

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os" // This package is used to interact with the operating system, including reading terminal input.
+)
+
 func main(){
-	
+	// os.Args is a built-in Go variable that contains a list of inputs from the terminal.
+    // Let's check: is the number of inputs less than 2? if so then throw an error
+    // (Index 0 = program name, Index 1 = username)
+	if len(os.Args) < 2 {
+		fmt.Println("Error: You must enter your username!")
+		fmt.Println("Example: go run main.go 'username'")
+	}
+
+	// if the input is correct, than take the second input (index 1)
+	username := os.Args[1]
+
+	// Print the result for testing
+	fmt.Printf("Searching github activity for user: %s/n",username)
 }
